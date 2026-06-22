11b318be2a7eb4fdeb68ebd605500dc03842bbb0
guideze <emilie.guidez@ucdenver.edu>
2026-06-22 13:00:48 -0600

    Script to extract molecular weight, molecular formula and SMILES strings from PubChem for a compound

---
+++ PubChemFetcher/PubChemFetcher.py
@@ -0,0 +1,16 @@
+import pubchempy as pcp
+#This script searches for molecular weight, molecular formula and SMILES string for a compound.
+# User needs to replace compound_name with the compound of interest.
+# Search PubChem for theobromine
+compound_name = 'theobromine'
+compounds = pcp.get_compounds(compound_name,'name')
+
+if compounds:
+    compound = compounds[0]
+
+    print(f"Name: {compound_name}")
+    print(f"Molecular Formula: {compound.molecular_formula}")
+    print(f"Molecular Weight: {compound.molecular_weight}")
+    print(f"Connectivity SMILES: {compound.connectivity_smiles}")
+else:
+    print("Theobromine not found in PubChem.")
